7a57e9bf765c5ad6359c250908190f8f982079eb
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2026-09-09 22:50:45 +0200

    Reworked hope fix the problem with the basic code .

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
-import nextra from 'nextra'
-
-const withNextra = nextra({})
-
-const isProd = process.env.NODE_ENV === 'production'
-
 export default withNextra({
   ...(isProd && { output: 'export' }),
   trailingSlash: true,
   images: { unoptimized: true },
+  ...(!isProd && {
+    async redirects() {
+      return [{ source: '/', destination: '/en', permanent: false }]
+    },
+  }),
   turbopack: {
     resolveAlias: {
       'next-mdx-import-source-file': './src/mdx-components.tsx',
     },
-  },
-  i18n: {
-    locales: ['en', 'nl'],
-    defaultLocale: 'en'
   },
   webpack(config) {
     config.resolve.alias['next-mdx-import-source-file'] =
