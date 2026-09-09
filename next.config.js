5937035225454c0fe6bba7ef8f7e163e66f4bbb6
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2026-09-09 23:02:24 +0200

    i haat AI v2

--- next.config.js
+++ next.config.js
@@ -8,15 +8,14 @@ export default withNextra({
   ...(isProd && { output: 'export' }),
   trailingSlash: true,
   images: { unoptimized: true },
-  ...(!isProd && {
-    async redirects() {
-      return [{ source: '/', destination: '/en', permanent: false }]
-    },
-  }),
   turbopack: {
     resolveAlias: {
       'next-mdx-import-source-file': './src/mdx-components.tsx',
     },
+  },
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'en'
   },
   webpack(config) {
     config.resolve.alias['next-mdx-import-source-file'] =
